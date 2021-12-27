adc9e38f4b0ad38c1ec21f5b27af9fdcea7b0ad7
Salim <doshka23@gmail.com>
2021-12-27 13:59:01 +0100

    responding to url params

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
+const fs = require('fs');
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.status(200).json({
-        message: 'Hello from the server side!',
-        app: 'Travicio'
+app.use(express.json());//will put the data coming from body in req object
+const profiles =JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/users.json`)) ;
 
+app.get('/api/v1/profiles', (req, res) =>{
+    res.status(200).json({
+        status:"success",
+        results: profiles.length,
+        data:{
+            profiles
+        }
     });
 });
+
+app.get('/api/v1/profile/:id', (req, res) =>{
+   
+    const id = req.params.id;
+
+   
+    const profile = profiles.find(el =>el._id === id );
+    //if(id > profiles.length){
+    if(!profile){
+        return res.status(404).json({
+            status: "fail",
+            message:"Invalid id"
+        })
+    }
+
+    res.status(200).json({
+        status:"success",
+        data:{
+            profile
+        } 
+    });
+});
+
+app.post('/api/v1/profiles', (req, res)=>{
+       console.log(req.body);
+       const newId = profiles[profiles.length - 1 ]._id + 1;
+       const newProfile = Object.assign({_id: newId}, req.body);
+       profiles.push(newProfile);
+       fs.writeFile(`${__dirname}/dev-data/data/users.json`, JSON.stringify(profiles), err =>{
+                res.status(201).json({
+                    status: "success",
+                    data:{
+                        profile: newProfile
+                    }
+                })
+       });
+       
+})
 
 
 
