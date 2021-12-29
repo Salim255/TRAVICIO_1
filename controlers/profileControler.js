28ccc8c9c1d8483aa55a2665ea7160a8a6d7ab06
Salim <doshka23@gmail.com>
2021-12-30 00:31:34 +0100

    Catching Errors in Async Functions

--- controlers/profileControler.js
+++ controlers/profileControler.js
@@ -1,68 +1,48 @@
 const Profile = require('../models/profileModel');
-const APIFeatures = require('../utils/apiFeatures')
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 //Routes handler
 exports.aliasTopProfiles = (req, res, next) =>{
-  req.query.limit = '5',
+  req.query.limit = '5';
   req.query.sort = '-ratingsAverage';
   next();
 }
 
 
 
-exports.getAllProiles =async (req, res) => {
-  try {
-      
-     //Excute query
-     const features = new APIFeatures(Profile.find(), req.query)
-     .filter()
-     .sort()
-     .limitFields().paginate();
-     
-     const profiles = await features.query;
+exports.getAllProiles = catchAsync(async (req, res, next) => {
+    //Excute query
+    const features = new APIFeatures(Profile.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields().paginate();
+    
+    const profiles = await features.query;
 
-     //Send response
-      res.status(200).json({
-        status: 'success',
-        results: profiles.length,
-        data: {
-          profiles,
-        }, 
-      });
-  } catch (error) {
-      res.status(404).json({
-        status: 'faild',
-        message: error
-      })
-  }
-};
+    //Send response
+     res.status(200).json({
+       status: 'success',
+       results: profiles.length,
+       data: {
+         profiles,
+       }, 
+     });
+});
 
 
 
-exports.createProfile = async (req, res) => {
-  try {
-    /* const newProfile = new Profile({
-  });
-  newProfile.save(); */
-
+exports.createProfile = catchAsync(async (req, res, next) => {
   const newProfile = await Profile.create(req.body);
   res.status(201).json({
     status: "success",
      data:{
         profile: newProfile
       } 
-    });
-   } catch (error) {
-     res.status(400).json({
-       status: 'faild',
-       message: error
-     })
-  }
-  
-}
+    }); 
+});
 
-exports.getProfile = async(req, res) =>{
-  try {
-     const profile = await Profile.findById(req.params.id);
+exports.getProfile = catchAsync(async(req, res, next) =>{
+  const profile = await Profile.findById(req.params.id);
  
      res.status(200).json({
        status: 'success',
@@ -70,96 +50,66 @@ exports.getProfile = async(req, res) =>{
          profile,
        },
      }); 
-   } catch (error) {
-        res.status(404).json({
-          status:'fail',
-          message: error
-        })
-   }
+ });
+
+exports.updateProfile = catchAsync(async (req, res, next) =>{
+  const profile = await Profile.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,//This willll send us the new updated document
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status:"success",
+    data: {
+        profile
+    }
+})    
+});
+
+exports.deletProfile =  catchAsync(async (req, res, next) =>{
    
- };
-
-exports.updateProfile = async (req, res) =>{
-   try {
-        const profile = await Profile.findByIdAndUpdate(req.params.id, req.body, {
-          new: true,//This willll send us the new updated document
-          runValidators: true
-        });
-
-        res.status(200).json({
-          status:"success",
-          data: {
-              profile
-          }
-      })
-   } catch (error) {
-      res.status(404).json({
-        status:'fail',
-        message: error
-      })
-   }
-        
-};
-
-exports.deletProfile =  async (req, res) =>{
-   
-  try {
-    await Profile.findByIdAndDelete(req.params.id);
-    res.status(200).json({
-      status:"success",
-      data: 
-        null
-      
-  })
-  } catch (error) {
-  res.status(404).json({
-    status:'fail',
-    message: error
-  })
- }
+  await Profile.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    status:"success",
+    data: 
+      null
     
-};
+})  
+});
 
-exports.getProfileStats = async (req, res) =>{
-  try {
-    const stats =  await Profile.aggregate([
-      {
-        $match:{
-          ratingsAverage:{ $gte: 4.5 }
+exports.getProfileStats = catchAsync(async (req, res, next) =>{
+  const stats =  await Profile.aggregate([
+    {
+      $match:{
+        ratingsAverage:{ $gte: 4.5 }
+      }
+    },
+    {
+        $group: {
+           _id:{$toUpper: '$jobStatus'} , 
+           numProfiles: { $sum: 1},
+           numRating: { $sum: '$ratingsQuantity'},
+           avgRating: { $avg: '$ratingsAverage'},
         }
-      },
+    },
+    {
+      $sort:{
+        avgRating :1
+      }
+    },
+   /*  {
+      $match: { _id: { $ne: 'BEAKER' } }
+    } */
+    
+  ]);
+  res.status(200).json({
+    status:"success",
+    data: 
       {
-          $group: {
-             _id:{$toUpper: '$jobStatus'} , 
-             numProfiles: { $sum: 1},
-             numRating: { $sum: '$ratingsQuantity'},
-             avgRating: { $avg: '$ratingsAverage'},
-          }
-      },
-      {
-        $sort:{
-          avgRating :1
-        }
-      },
-     /*  {
-        $match: { _id: { $ne: 'BEAKER' } }
-      } */
-      
-    ]);
-    res.status(200).json({
-      status:"success",
-      data: 
-        {
-          stats
-        }
-      
-  })
-  } catch (error) {
-    res.status(404).json({
-      status:'fail',
-      message: error
-    })
-   }
-}
+        stats
+      }
+    
+})
+});
 
 
