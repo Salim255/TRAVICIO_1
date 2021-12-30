4e697edb733df8a67ae239b0c5eab64ea7be34eb
Salim <doshka23@gmail.com>
2021-12-30 02:55:32 +0100

    Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ mongoose.connect(DB, {
 //console.log(app.get('env'));
 //We start the server
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App runing on port ${port}...`);
 });
+
+
+process.on('unhandledRejection', err =>{
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() =>{
+    process.exit(1);
+  });
+  
+})
