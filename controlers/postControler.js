7ed436c33f35c17390744536f5f4017c218867a8
Salim <doshka23@gmail.com>
2022-01-04 02:43:09 +0100

    delte and add comment , like and unlike delete and add post ..

--- controlers/postControler.js
+++ controlers/postControler.js
@@ -1,10 +1,191 @@
 const Profile = require('../models/profileModel');
+const User = require('../models/userModel');
+const Post  =  require('../models/postModel');
+
 const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const router = require('../routes/postRoute');
 
-exports.creatpost = catchAsync(async (req, res) =>{
-     console.log('====================================');
-     console.log("Hello 💥");
-     console.log('====================================');
+//POST api/v1/posts
+//Creat a userPost
+//Privite
+exports.creatpost = catchAsync(async (req, res, next) =>{
+     const user = await User.findById(req.user.id);
+
+     const newPost = new Post({
+          text: req.body.text,
+          name: user.name,
+          avatar: user.avatar,
+          user: req.user.id
+     });
+
+
+     const post = await newPost.save();
+     res.status(200).json({
+          status: 'success',
+          data:{
+               post
+          }
+     })
 });
+
+//GET api/v1/posts
+//Get all posts
+//Privite
+exports.getAllPosts = catchAsync(async (req, res, next) =>{
+     const posts = await Post.find().sort({data: -1});
+     res.status(200).json({
+          status: 'success',
+          data:{
+               posts
+          }
+     })
+});
+
+//Get api/posts/:id
+//Get post by post id
+//Privite
+exports.getPostById = catchAsync( async(req, res, next) => {
+     
+     const post = await Post.findById(req.params.id);
+
+     if(!post){
+          return next(new AppError('Post not found', 404));
+     }
+
+     res.status(200).json({
+          status: 'success',
+          data:{
+               post
+          }
+     })
+});
+
+//delete api/posts/:id
+//delete post by Id
+//Privite
+exports.deletePostById  = catchAsync(async(req, res, next) =>{
+     const post = await Post.findById(req.params.id);
+     if(!post){
+          return next( new AppError('Post not found', 404));
+      }
+
+      //Check user
+      if(post.user.toString() !== req.user.id){
+          return res.status(401).json({ 
+               status: 'fail',
+               message: 'User not authorized'
+           });
+     }
+
+     await post.remove();
+
+     res.status(200).json({
+          status: 'success',
+          message:' Post removed'
+     })
+});
+
+//PUT api/posts/like/:id
+//Like a post
+//Privite
+exports.likePostById = catchAsync(async(req, res, next) =>{
+     const post = await Post.findById(req.params.id);
+       //Ckeck if the post has already been liked
+     if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+          return res.status(400).json({ message: 'Post already liked' });
+      }
+      post.likes.unshift({ user: req.user.id });//put in the beggining 
+      await post.save();
+      res.status(200).json({
+           status: 'success',
+           data:{
+              data: post.likes
+           }
+      });
+})
+
+//Put api/posts/unlike/:id
+//UNLike a post
+//Privite
+exports.unlikePostById = catchAsync( async(req, res, next) =>{
+     const post = await Post.findById(req.params.id);
+
+     //Ckeck if the post has already been liked
+     if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+          return res.status(400).json({ message: 'Post has not yet been liked' });
+      }
+      //Get remove index
+      const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+      post.likes.splice(removeIndex, 1);
+      
+      await post.save();
+      res.status(200).json(post.likes);
+});
+
+//1route POST api/posts/comment/:id
+//2description Comment on  a post 
+//3access Private
+exports.addCommentOnPost = catchAsync( async(req, res, next) => {
+   
+     const user = await User.findById(req.user.id);
+     const post = await Post.findById(req.params.id);
+
+     const newComment = new Post({
+          text: req.body.text,
+          name: user.name,
+          avatar: user.avatar,
+          user: req.user.id
+     }) ;
+     post.comments.unshift(newComment);
+
+     await post.save();
+
+     res.status(200).json({
+          status: 'success',
+          data:{
+              data: post.comments
+          }
+     });
+});
+
+//1route DELET api/posts/comment/:id/:comment_id
+//2description delete Comment on  a post 
+//3access Private
+
+exports.deleteCommentOnPostById = catchAsync(async(req, res, next) =>{
+       const post = await Post.findById(req.params.id);
+
+        //PULL out comment from the post
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+        
+        //Make sure comment exist
+        if(!comment){
+            return res.status(404).json({
+                status: 'fail',
+                message: 'Comment does not exist'
+            })
+        };
+
+        //Check User
+        if(comment.user.toString() !== req.user.id){
+            return res.status(401).json({
+                status: 'fail',
+                message: 'User not authorized'
+            });
+        }
+
+        //Get remove index
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+        
+        await post.save();
+        res.status(200).json({
+             status: 'success',
+             data:{
+               data: post.comments
+             }
+        });
+})
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,7 +5,24 @@ const router = express.Router();
 const authControler = require('../controlers/authControler');
 const postControler = require('../controlers/postControler');
 
-router.route('/').post(postControler.creatpost);
+router.route('/')
+.post(authControler.protect, postControler.creatpost)
+.get(authControler.protect,  postControler.getAllPosts)
+;
 
+
+router.route('/like/:id')
+    .get(authControler.protect, postControler.getPostById )
+    .delete(authControler.protect, postControler.deletePostById)
+    .put(authControler.protect, postControler.likePostById);
+
+router.route('/unlike/:id')
+    .put(authControler.protect, postControler.unlikePostById);
+
+router.route('/comment/:id')
+    .put(authControler.protect, postControler.addCommentOnPost)
+
+router.route('/comment/:id/:comment_id')
+    .delete(authControler.protect, postControler.deleteCommentOnPostById );
 
 module.exports = router;
